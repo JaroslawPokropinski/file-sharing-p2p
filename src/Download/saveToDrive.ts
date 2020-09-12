3eb44bfdb0cfe6cd488276bf5b7843bea2c72e60
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-09-12 23:50:47 +0200

    First deploy

--- src/Download/saveToDrive.ts
+++
@@ -1,9 +0,0 @@
-const saveToDrive = (name: string, buffer: Buffer, w = window, d = document) => {
-  const url = w.URL.createObjectURL(new Blob([new Uint8Array(buffer, 0, buffer.length)]));
-  const tempLink = d.createElement('a');
-  tempLink.href = url;
-  tempLink.setAttribute('download', name);
-  tempLink.click();
-};
-
-export default saveToDrive;
--- src/Share/Share.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Share from './Share';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Share />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
